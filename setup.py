a5553ecb748e80366ea10872c5f6f87217520d35
Hewlette Dibble <hcodydibble@gmail.com>
2017-12-01 11:16:18 -0800

    updated setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name="data-structures",
     description="Data structures.",
-    py_modules=['linked_list'],
+    py_modules=['dll', 'deque', 'bst'],
     install_requires=[],
     extras_require={'test': ['pytest', 'pytest-watch', 'tox']},
     package_dir={"": "src"}
