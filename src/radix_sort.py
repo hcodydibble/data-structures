81603c0312c425c36c043b38b7112ce86a4a6d19
Hewlette Dibble <hcodydibble@gmail.com>
2017-12-13 09:08:48 -0800

    if name equal main block

--- src/radix_sort.py
+++ src/radix_sort.py
@@ -22,3 +22,20 @@ def radix_sort(a_list):
         return output
     else:
         raise ValueError("Lists only")
+
+if __name__ == '__main__':  # pragma: no cover
+    import timeit
+    from random import randint
+    short_list = [randint(1, 50) for _ in range(10)]
+    long_list = [randint(1, 50) for _ in range(100)]
+    small_numbers = [randint(1, 25) for _ in range(15)]
+    big_numbers = [randint(101, 250) for _ in range(15)]
+
+    short_list = timeit.timeit("radix_sort(short_list)", setup="from __main__ import short_list, radix_sort")
+    long_list = timeit.timeit("radix_sort(long_list)", setup="from __main__ import long_list, radix_sort")
+    small_numbers = timeit.timeit("radix_sort(small_numbers)", setup="from __main__ import small_numbers, radix_sort")
+    big_numbers = timeit.timeit("radix_sort(big_numbers)", setup="from __main__ import big_numbers, radix_sort")
+    print('Short list time: ', short_list)
+    print('Long list time: ', long_list)
+    print('Small number list time: ', small_numbers)
+    print('Big numbers list time:', big_numbers)
