e012a6b12d3b675bbbc1c2e7cd9b84a5bb38a774
Hewlette Dibble <hcodydibble@gmail.com>
2017-12-15 17:38:42 -0800

    added other py_modules

--- setup.py
+++ setup.py
@@ -1,9 +1,23 @@
 from setuptools import setup
 
+MODULES = ['bst',
+'bubble_sort',
+'deque',
+'dll',
+'hashtable',
+'insertion_sort',
+'linked_list',
+'merge_sort',
+'priorityq',
+'que_',
+'quick_sort',
+'radix_sort',
+'trie']
+
 setup(
     name="data-structures",
     description="Data structures.",
-    py_modules=['dll', 'deque', 'bst'],
+    py_modules=MODULES,
     install_requires=[],
     extras_require={'test': ['pytest', 'pytest-watch', 'tox']},
     package_dir={"": "src"}
